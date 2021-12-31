3d0f5a2f3fc4e0bd8c0804cf2ce7cf0a22697b73
Moto Sakanosita <moto@sakanosita.org>
2021-12-31 12:42:22 +0900

    update: CSS modules

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import * as headerStyles from "./header.module.scss"
+
 const Header = () => {
     return (
-        <header>
-            <h1>Gatsby Bootcamp</h1>
+        <header className={headerStyles.header}>
+            <h1>
+                <Link className={headerStyles.title} to="/">Gatsby Bootcamp</Link>
+            </h1>
             <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
+                <ul className={headerStyles.navList}>
                     <li>
-                        <Link to="/blog">Blog</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link>
                     </li>
                     <li>
-                        <Link to="/about">About</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link>
                     </li>
                     <li>
-                        <Link to="/contact">Contact</Link>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">Contact</Link>
                     </li>
                 </ul>
             </nav>
