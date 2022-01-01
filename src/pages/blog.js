99ea8ee1da8ba2fad8f22ddeb9d032e4de3ebe9c
Moto Sakanosita <moto@sakanosita.org>
2022-01-01 15:36:14 +0900

    chore

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../components/layout'
 import { graphql, useStaticQuery, Link } from 'gatsby'
+import * as blogStyles from './blog.module.scss'
 
 const BlogPage = () => {
     const data = useStaticQuery(graphql`
@@ -24,13 +25,13 @@ const BlogPage = () => {
     return (
         <Layout>
             <h1>Blog</h1>
-            <ol>
+            <ol className={blogStyles.posts}>
                 {data.allMarkdownRemark.edges.map((edge) => {
                     return (
-                    <li>
+                    <li className={blogStyles.post}>
                         <Link to={`/blog/${edge.node.fields.slug}`}>
-                        <h2>{edge.node.frontmatter.title}</h2>
-                        <p>{edge.node.frontmatter.date}</p>
+                            <h2>{edge.node.frontmatter.title}</h2>
+                            <p>{edge.node.frontmatter.date}</p>
                         </Link>
                     </li>
                     )
--- src/posts/gatsby/gatsby.md
+++ src/posts/gatsby/gatsby.md
@@ -5,10 +5,10 @@ date: "2021-12-31"
 
 I just launched a new bootcamp!
 
+![Grass](./grass.jpeg)
+
 ### Topics Covered
 
 1. Gatsby
 2. GraphQL
 3. React
-
-![Grass](./grass.jpeg)
